0b5a6b1d3518cf09d2483f057a314cf68cc1bf6d
Woeldhin <32248067+Woeldhin@users.noreply.github.com>
2019-04-08 01:52:43 +0300

    Added jumping
    Press space to jump
    Set jump force to your liking
    Tried doing drawer pulling, but that's really hard.

---
+++ Assets/Scripts/DrawerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerController : MonoBehaviour
+{
+
+    private float minPull;
+    private float maxPull;
+    private float oldTangent;
+    public float pull;
+    public float currentPoint;
+
+    void Start()
+    {
+        minPull = transform.position.z;
+        maxPull = minPull - pull;
+        oldTangent = 0;
+    }
+
+    void Hold(Vector3 lookPoint)
+    {
+        if (oldTangent != 0)
+        {
+            // Shrug
+
+            if (currentPoint <= minPull && currentPoint >= maxPull)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, currentPoint);
+            }
+        }
+        else
+        {
+            oldTangent = lookPoint.x - transform.position.x;
+        }
+        
+    }
+
+}
