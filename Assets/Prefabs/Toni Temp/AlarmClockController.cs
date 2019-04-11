948a81b611f691fa4d54c46dc2a597dc2e78b726
Tonihuht <33083428+Tonihuht@users.noreply.github.com>
2019-04-11 12:05:48 +0300

    Sounds and alarmclock temp
    Just quick push to get all needed stuff to project and start getting it together at unity before committing final

---
+++ Assets/Prefabs/Toni Temp/AlarmClockController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmClockController : MonoBehaviour
+{
+
+    public GameObject minuteScroll;
+    public GameObject hourScroll;
+
+    public GameObject minutePointer;
+    public GameObject hourPointer;
+
+    private float tiltAngle = -30.0f;
+
+    void Interact()
+    {
+        print("Beep beep beep, beep beep beep!");
+        if (gameObject == minuteScroll)
+        {
+            minutePointer.transform.Rotate(0, tiltAngle, 0);
+        }
+        if (gameObject == hourScroll)
+        {
+            hourPointer.transform.Rotate(0, tiltAngle, 0);
+        }
+    }
+
+    void Update()
+    {
+        if(minutePointer.transform.localRotation.y == 0 && hourPointer.transform.localRotation.y == 120)
+        {
+            Debug.Log("Voitit kellopelin!");
+        }
+    }
+}
