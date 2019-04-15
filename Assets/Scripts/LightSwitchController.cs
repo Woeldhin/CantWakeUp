48cc71c91384f711d24c31e21a6223102afded61
Tonihuht <33083428+Tonihuht@users.noreply.github.com>
2019-04-15 15:42:55 +0300

    LightSwitch ready and working
    Way to turn lights off and on for hidden message puzzle, and nice feature either way to have .  [Sample Scene]

---
+++ Assets/Scripts/LightSwitchController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSwitchController : MonoBehaviour
+{
+
+    public bool isLightsOn;
+
+
+
+    public GameObject SpotLight;
+    public GameObject Switch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(SpotLight.activeInHierarchy == true)
+        {
+            isLightsOn = true;
+            Switch.transform.eulerAngles = new Vector3(10, 0, 0);
+        }
+        else
+        {
+            isLightsOn = false;
+            Switch.transform.eulerAngles = new Vector3(-10, 0, 0);
+        }
+    }
+
+    void Interact()
+    {
+        Debug.Log("LightSwitch!");
+        if(isLightsOn == true)
+        {
+            isLightsOn = false;
+        }
+        else
+        {
+            isLightsOn = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isLightsOn == true)
+        {
+            SpotLight.SetActive(true);
+        }
+        else
+        {
+            SpotLight.SetActive(false);
+        }
+    }
+}
