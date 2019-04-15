d75d05aad704e1d074fe1ea32b4ff25d6d6a95ca
ferrylehtinen <ferry.lehtinen@metropolia.fi>
2019-04-15 19:02:41 +0300

    Water puzzle proto tehty

---
+++ Assets/Scripts/WaterPuzzleReset.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPuzzleReset : MonoBehaviour
+
+{
+    GameObject tap;
+    GameObject tank3;
+    GameObject tank5;
+
+
+    Color color = Color.white;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tap = GameObject.Find("Tap");
+        tank5 = GameObject.Find("Tank5");
+        tank3 = GameObject.Find("Tank3");
+    }
+
+    void Interact()
+    {
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("WaterPuzzleBars");
+        foreach (GameObject go in objects)
+        {
+            Renderer[] renderers = go.GetComponents<Renderer>();
+            foreach (Renderer r in renderers)
+            {
+                foreach (Material m in r.materials)
+                {
+                    if (m.HasProperty("_Color"))
+                        m.color = color;
+                }
+            }
+        }
+
+        GameObject.Find("Tank5").GetComponent<WaterPuzzleTank5>().count5 = 0;
+        GameObject.Find("Tank3").GetComponent<WaterPuzzleTank3>().count3 = 0;
+
+        tap.GetComponent<Renderer>().material.color = Color.white;
+        tank3.GetComponent<Renderer>().material.color = Color.white;
+        tank5.GetComponent<Renderer>().material.color = Color.white;
+
+    }
+         
+
+}
---
+++ Assets/Scripts/WaterPuzzleTank3.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPuzzleTank3 : MonoBehaviour
+{
+    GameObject tap;
+    GameObject tank5;
+
+    GameObject tank3bar1;
+    GameObject tank3bar2;
+    GameObject tank3bar3;
+
+    GameObject tank5bar1;
+    GameObject tank5bar2;
+    GameObject tank5bar3;
+    GameObject tank5bar4;
+    GameObject tank5bar5;
+
+    public int script5count;
+    public int realcount;
+    public int rest;
+
+    public int count3 = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tap = GameObject.Find("Tap");
+        tank5 = GameObject.Find("Tank5");
+
+        tank3bar1 = GameObject.Find("Tank3Bar1");
+        tank3bar2 = GameObject.Find("Tank3Bar2");
+        tank3bar3 = GameObject.Find("Tank3Bar3");
+
+        tank5bar1 = GameObject.Find("Tank5Bar1");
+        tank5bar2 = GameObject.Find("Tank5Bar2");
+        tank5bar3 = GameObject.Find("Tank5Bar3");
+        tank5bar4 = GameObject.Find("Tank5Bar4");
+        tank5bar5 = GameObject.Find("Tank5Bar5");
+
+        script5count = GameObject.Find("Tank5").GetComponent<WaterPuzzleTank5>().count5;
+    }
+
+    void Interact()
+    {
+        if (gameObject.GetComponent<Renderer>().material.color == Color.red)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if ((gameObject.GetComponent<Renderer>().material.color == Color.red) && (tap.GetComponent<Renderer>().material.color == Color.red))
+        {
+            count3 = 3;
+            tank3bar1.GetComponent<Renderer>().material.color = Color.yellow;
+            tank3bar2.GetComponent<Renderer>().material.color = Color.yellow;
+            tank3bar3.GetComponent<Renderer>().material.color = Color.yellow;
+
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            tap.GetComponent<Renderer>().material.color = Color.white;
+
+        }
+
+        if ((gameObject.GetComponent<Renderer>().material.color == Color.red) && (tank5.GetComponent<Renderer>().material.color == Color.red))
+        {
+            realcount = count3 + GameObject.Find("Tank5").GetComponent<WaterPuzzleTank5>().count5;
+
+            if (realcount > 3)
+            {
+                rest = realcount - 3;
+                realcount -= rest;
+            }
+
+            switch (realcount)
+            {
+                case 3:
+                    tank3bar1.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank3bar2.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank3bar3.GetComponent<Renderer>().material.color = Color.yellow;
+                    break;
+                case 2:
+                    tank3bar3.GetComponent<Renderer>().material.color = Color.white;
+                    tank3bar2.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank3bar1.GetComponent<Renderer>().material.color = Color.yellow;
+                    break;
+                case 1:
+                    tank3bar3.GetComponent<Renderer>().material.color = Color.white;
+                    tank3bar2.GetComponent<Renderer>().material.color = Color.white;
+                    tank3bar1.GetComponent<Renderer>().material.color = Color.yellow;
+                    break;
+                case 0:
+                    tank3bar1.GetComponent<Renderer>().material.color = Color.white;
+                    tank3bar2.GetComponent<Renderer>().material.color = Color.white;
+                    tank3bar3.GetComponent<Renderer>().material.color = Color.white;
+                    break;
+                default:
+                    print("Error");
+                    break;
+            }
+
+            count3 = realcount;
+
+            GameObject.Find("Tank5").GetComponent<WaterPuzzleTank5>().count5 = rest;
+
+            switch (rest)
+            {
+
+                case 5:
+                    tank5bar5.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar4.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar3.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar2.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar1.GetComponent<Renderer>().material.color = Color.yellow;
+                    break;
+                case 4:
+                    tank5bar5.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar4.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar3.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar2.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar1.GetComponent<Renderer>().material.color = Color.yellow;
+                    print("You win!");
+                    break;
+                case 3:
+                    tank5bar5.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar4.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar3.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar2.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar1.GetComponent<Renderer>().material.color = Color.yellow;
+                    break;
+                case 2:
+                    tank5bar5.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar4.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar3.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar2.GetComponent<Renderer>().material.color = Color.yellow;
+                    tank5bar1.GetComponent<Renderer>().material.color = Color.yellow;
+                    break;
+                case 1:
+                    tank5bar5.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar4.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar3.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar2.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar1.GetComponent<Renderer>().material.color = Color.yellow;
+                    break;
+                case 0:
+                    tank5bar1.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar2.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar3.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar4.GetComponent<Renderer>().material.color = Color.white;
+                    tank5bar5.GetComponent<Renderer>().material.color = Color.white;
+                    break;
+                default:
+                    print("Error");
+                    break;
+            }
+
+            rest = 0;
+
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            tank5.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+
+}
