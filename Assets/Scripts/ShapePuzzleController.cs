5013e8a8dec4e35f68594ca77f57915a52a2c23e
Tonihuht <33083428+Tonihuht@users.noreply.github.com>
2019-04-22 00:37:41 +0300

    ShapePuzzle ready
    Added script "ShapePuzzleController" that check if condition to win puzzle are met and if yes then gives a message that you won the puzzle

--- Assets/Scripts/ShapePuzzleController.cs
+++ Assets/Scripts/ShapePuzzleController.cs
@@ -4,12 +4,56 @@ using UnityEngine;
 
 public class ShapePuzzleController : MonoBehaviour
 {
-    
+    bool isTriangleDone;
+    bool isSquareDone;
+    bool isCircleDone;
+
+    void Start()
+    {
+
+        isTriangleDone = false;
+        isSquareDone = false;
+        isCircleDone = false;
+
+    }
+
     // Update is called once per frame
     void Update()
     {
+        //Check if triangle object exists
+        if (GameObject.Find("TrianglePiece"))
+        {
+            isTriangleDone = false;
+        }
+        else
+        {
+            isTriangleDone = true;
+        }
 
+        //Check if square object exists
+        if (GameObject.Find("SquarePiece"))
+        {
+            isSquareDone = false;
+        }
+        else
+        {
+            isSquareDone = true;
+        }
 
+        //Check if circle object exists
+        if (GameObject.Find("CirclePiece"))
+        {
+            isCircleDone = false;
+        }
+        else
+        {
+            isCircleDone = true;
+        }
 
+        //Checks if the puzzle is solved fully (all 3 pieces put to place) = win the puzzle
+        if (isTriangleDone == true && isSquareDone == true && isCircleDone == true)
+        {
+            Debug.Log("You won the shape puzzle");
+        }
     }
 }
