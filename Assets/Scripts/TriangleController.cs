672c9efe0b8ed590a88f8a0e416ea2c4d8a29e6b
Tonihuht <33083428+Tonihuht@users.noreply.github.com>
2019-04-22 00:00:58 +0300

    ShapePuzzle halfwaydone
    Collision detection works and pieces vanish when collided with right place. Puzzle solution not done yet and it is next to come .

---
+++ Assets/Scripts/TriangleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleController : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "TrianglePiece")
+        {
+            Debug.Log("TriangleHit");
+            Destroy(col.gameObject);
+        }
+    }
+}
