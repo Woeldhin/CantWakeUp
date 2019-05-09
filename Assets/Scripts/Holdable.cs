5c5d2df4c5f5fe132fee71767964f7f1394f27cf
Woeldhin <32248067+Woeldhin@users.noreply.github.com>
2019-05-09 23:42:41 +0300

    Sure. Try this

--- Assets/Scripts/Holdable.cs
+++ Assets/Scripts/Holdable.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Holdable : MonoBehaviour
 {
     public Rigidbody rb;
+    public float pullForce;
     private Vector3 velocity;
     private Vector3 lastFramePosition;
 
@@ -12,19 +13,27 @@ public class Holdable : MonoBehaviour
     {
         rb.freezeRotation = true;
         lastFramePosition = transform.position;
+        rb.useGravity = false;
     }
 
     void Hold(Vector3 grabPoint)
     {
         velocity = (transform.position - lastFramePosition) / (Time.deltaTime * 2);
         lastFramePosition = transform.position;
-        transform.position = grabPoint;
-        transform.LookAt(grabPoint);
+        rb.AddForce((grabPoint - transform.position) * pullForce * Vector3.Distance(transform.position, grabPoint));
+        if(Vector3.Distance(transform.position, grabPoint) < 0.05f)
+        {
+            rb.velocity *= Vector3.Distance(transform.position, grabPoint);
+        }
+        
+        //transform.position = grabPoint;
+        //transform.LookAt(grabPoint);
     }
 
     void Reset()
     {
         rb.velocity = velocity;
         rb.freezeRotation = false;
+        rb.useGravity = true;
     }
 }
