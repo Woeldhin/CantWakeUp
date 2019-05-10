3ce845425e41e1dd5803e974baf019f5209fc1d4
Sam Hemming <Saatanastaseuraava@gmail.com>
2019-05-10 07:17:41 +0300

    The Final Push, PLZ

--- Assets/Scripts/AlarmClockController.cs
+++ Assets/Scripts/AlarmClockController.cs
@@ -2,73 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AlarmClockController : MonoBehaviour
 {
     // Drawer puzzle starter
     public DresserController dresser;
     public DrawerController drawer;
 
-    //Scrolls at back of clock that player uses to change clocks time
-    public GameObject minuteScroll;
-    public GameObject hourScroll;
-
     //Imaginary "pointer" that show us time, really just used as pivot points to originally check the rotation of pointers to solve this puzzle, now pretty useless, but it 'kay
     public GameObject minutePointer;
     public GameObject hourPointer;
 
+    public GameObject correctMinutePointer;
+    public GameObject correctHourPointer;
+
     //manually set counters to check what time clock is showing right now , also needed to check if player has solved the puzzle
-    public int hourCounter = 7;
-    public int minuteCounter = 0;
+    private int hourCounter;
+    private int minuteCounter;
+
+    public AudioSource AlarmSource;
+    //Audio stuff//
+    //AudioClips
+    public AudioClip buttonSoundFail;
+    public AudioClip buttonSoundSuccess;
+    //AudioSources
+    public AudioSource buttonSourceFail;
+    public AudioSource buttonSourceSuccess;
 
     [HideInInspector]
-    public int hourSuccess = 3;
+    public int hourSuccess;
     [HideInInspector]
-    public int minuteSuccess = 0;
+    public int minuteSuccess;
 
     //how much will pointers tilt from one click of the "scrolls"
     private float tiltAngle = -30.0f;
 
-    void Interact()
+    private void Start()
     {
-        print("Beep beep beep, beep beep beep!");
-        //turning of minute pointer and keeping minuteCounter at right number
-        if (gameObject == minuteScroll)
-        {
-            Debug.Log("MinutesTurning");
-            minutePointer.transform.Rotate(0, tiltAngle, 0);
-            if (minuteCounter < 11)
-            {
-                Debug.Log("MinutesTurning");
-                minuteCounter++;
-            }
-            else
-            {
-                minuteCounter = 0;
-            }
-            
-        }
+        minuteCounter = Random.Range(0, 11);
+        hourCounter = Random.Range(0, 11);
+        minuteSuccess = Random.Range(0, 11);
+        hourSuccess = Random.Range(0, 11);
 
-        //turning of hour pointer and keeping hourCounter at right number
-        if (gameObject == hourScroll)
-        {
-            Debug.Log("HoursTurning");
-            hourPointer.transform.Rotate(0, tiltAngle, 0);
-            if (hourCounter < 11)
-            {
-                hourCounter++;
-            }
-            else
-            {
-                hourCounter = 0;
-            }
-        }
+        correctHourPointer.transform.localEulerAngles = new Vector3(0, 0, hourSuccess * tiltAngle);
+        correctMinutePointer.transform.localEulerAngles = new Vector3(0, 0, minuteSuccess * tiltAngle);
 
-        //Check if minute and hour counters are at right position for player to "win" this puzzle
-        if (minuteCounter == minuteSuccess && hourCounter == hourSuccess)
+        hourPointer.transform.localEulerAngles = new Vector3(0, hourCounter * tiltAngle);
+        minutePointer.transform.localEulerAngles = new Vector3(0, minuteCounter * tiltAngle);
+
+        buttonSourceFail.clip = buttonSoundFail;
+        buttonSourceSuccess.clip = buttonSoundSuccess;
+    }
+
+    public void ButtonFeed(AlarmButtonType alarmButtonType)
+    {
+        switch (alarmButtonType)
         {
-            Debug.Log("Voitit kellopelin!");
-            drawer.TogglePosition();
-            dresser.puzzleActive = true;
+            case AlarmButtonType.minute:
+                if (minuteCounter < 11)
+                {
+                    minuteCounter++;
+                }
+                else
+                {
+                    minuteCounter = 0;
+                }
+                minutePointer.transform.localEulerAngles = new Vector3(0, minuteCounter * tiltAngle);
+                break;
+            case AlarmButtonType.hour:
+                if (hourCounter < 11)
+                {
+                    hourCounter++;
+                }
+                else
+                {
+                    hourCounter = 0;
+                }
+                hourPointer.transform.localEulerAngles = new Vector3(0, hourCounter * tiltAngle);
+                break;
+            case AlarmButtonType.button:
+                //Check if minute and hour counters are at right position for player to "win" this puzzle
+                if (minuteCounter == minuteSuccess && hourCounter == hourSuccess)
+                {
+                    Debug.Log("Voitit kellopelin!");
+                    drawer.TogglePosition();
+                    dresser.puzzleActive = true;
+                    buttonSourceSuccess.Play();
+                    Destroy(AlarmSource);
+                } else
+                {
+                    buttonSourceFail.Play();
+                }
+                break;
+            default:
+                break;
         }
     }
+
 }
---
+++ Assets/Scripts/AlarmClockScroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum AlarmButtonType
+{
+    minute, hour, button
+}
+
+public class AlarmClockScroll : MonoBehaviour
+{
+    public AlarmButtonType alarmButtonType;
+    private AlarmClockController alarmClockController;
+
+    private void Start()
+    {
+        transform.tag = "Interactable";
+        alarmClockController = GetComponentInParent<AlarmClockController>();
+    }
+
+    void Interact()
+    {
+        alarmClockController.ButtonFeed(alarmButtonType);
+    }
+
+
+}
